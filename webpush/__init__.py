2e16e6b64102d3712ff90e73818f13d89c5cba51
Safwan Rahman <safwan.rahman15@gmail.com>
2016-04-29 20:48:37 +0600

    correcting some errors

--- webpush/__init__.py
+++ webpush/__init__.py
@@ -4,10 +4,10 @@ from .utils import send_notification_to_group, send_notification_to_user
 
 
 def send_group_notification(group_name, payload, ttl=0):
-    payload = json.dump(payload)
+    payload = json.dumps(payload)
     send_notification_to_group(group_name, payload, ttl)
 
 
 def send_user_notification(user, payload, ttl=0):
-    payload = json.dump(payload)
+    payload = json.dumps(payload)
     send_notification_to_user(user, payload, ttl)
