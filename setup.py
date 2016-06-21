af0e4ec48fc99371a8888067d2646117d47c6434
Safwan Rahman <safwan.rahman15@gmail.com>
2016-06-21 07:32:40 +0600

    Bumping version 2.0.0

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
     name='django-webpush',
-    version='0.1.3',
+    version='0.2.0',
     packages=find_packages(),
     include_package_data=True,
     license='GNU Public License',
