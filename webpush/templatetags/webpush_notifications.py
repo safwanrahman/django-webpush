c2428f4393c405079a5914fb43fad86f88288d8d
Alexey Rogachev <arogachev90@gmail.com>
2016-10-23 00:52:54 +0600

    Fixed #16: Better way to store webpush data in DOM (#18)

--- webpush/templatetags/webpush_notifications.py
+++ webpush/templatetags/webpush_notifications.py
@@ -1,6 +1,4 @@
 from django import template
-from django.conf import settings
-from django.core.exceptions import ObjectDoesNotExist
 from django.core.urlresolvers import reverse
 
 register = template.Library()
@@ -9,23 +7,15 @@ register = template.Library()
 @register.filter
 @register.inclusion_tag('webpush.html', takes_context=True)
 def webpush(context):
-  group = context.get("webpush", {}).get("group")
-
-  url = reverse('save_webpush_info')
-  request = context["request"]
-
-  return { 'group': group,
-           'url': url,
-           "request": request
-          }
+    group = context.get('webpush', {}).get('group')
+    request = context['request']
+    return {'group': group, 'request': request}
 
 
 @register.filter
 @register.inclusion_tag('webpush_button.html', takes_context=True)
 def webpush_button(context):
-  group = context.get("webpush", {}).get("group")
-  request = context["request"]
-
-  return {"group": group,
-          "request": request
-         }
+    group = context.get('webpush', {}).get('group')
+    url = reverse('save_webpush_info')
+    request = context['request']
+    return {'group': group, 'url': url, 'request': request}
