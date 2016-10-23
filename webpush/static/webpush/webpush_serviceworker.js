3908e22ad9af1d7d66b0b79a4bc2bc07daff4959
Justin Duke <me@jmduke.com>
2016-10-22 18:47:49 -0700

    Act on notification clicks.

--- webpush/static/webpush/webpush_serviceworker.js
+++ webpush/static/webpush/webpush_serviceworker.js
@@ -16,6 +16,22 @@ self.addEventListener('push', function(event) {
 });
 
 self.addEventListener('notificationclick', function(event) {
-  console.log('On notification click: ', event.notification.tag);
-  event.notification.close();
+    event.notification.close();
+    var url = event.notification.data.url;
+    event.waitUntil(
+        clients.matchAll({
+            type: 'window'
+        })
+        .then(function(windowClients) {
+            for (var i = 0; i < windowClients.length; i++) {
+                var client = windowClients[i];
+                if (client.url === url && 'focus' in client) {
+                    return client.focus();
+                }
+            }
+            if (clients.openWindow) {
+                return clients.openWindow(url);
+            }
+        })
+    );
 });
