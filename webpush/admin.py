9d88afe6c97133ccb492819bf17a2fa8431cea44
Jamaal Scarlett <jamaal.scarlett@gmail.com>
2017-03-29 11:28:46 -0400

    Register PushInfo with django admin. (#32)
    * Register PushInfo with django admin.
    Create send_message test method.
    
    * Change method to send_test_message.
    Use python dict to store test message
    
    * Use int, not string for ttl param.

--- webpush/admin.py
+++ webpush/admin.py
@@ -1,3 +1,19 @@
+import json
+
 from django.contrib import admin
 
-# Register your models here.
+from .models import PushInformation
+from .utils import _send_notification
+
+
+class PushInfoAdmin(admin.ModelAdmin):
+    list_display = ("__str__", "user", "subscription", "group")
+    actions = ("send_test_message",)
+
+    def send_test_message(self, request, queryset):
+        result = []
+        payload = {"head": "Hey", "body": "Hello World"}
+        for device in queryset:
+            result.append(_send_notification(device, json.dumps(payload), 0))
+
+admin.site.register(PushInformation, PushInfoAdmin)
