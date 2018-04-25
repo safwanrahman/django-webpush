0f19a07399b01189947aaba5c1e91f3f91c31e27
Lucas <jlucasrsantana@gmail.com>
2018-04-25 13:24:23 -0300

    README updated.

--- webpush/templates/webpush_serviceworker.js
+++ webpush/templates/webpush_serviceworker.js
@@ -4,13 +4,14 @@ self.addEventListener('push', function(event) {
   // Retrieve the textual payload from event.data (a PushMessageData object).
   // Other formats are supported (ArrayBuffer, Blob, JSON), check out the documentation
   // on https://developer.mozilla.org/en-US/docs/Web/API/PushMessageData.
-  let payload = event.data ? event.data.text() : {"head": "No Content", "body": "No Content", "icon": "", "url": ""},
+  let payload = event.data ? event.data.text() : {"head": "No Content", "body": "No Content", "icon": ""},
     data = JSON.parse(payload),
     head = data.head,
     body = data.body,
     icon = data.icon;
-  // Url needs to be acessed from outside this method.
-  url = data.url
+    // If no url was received, it opens the home page of the website that sent the notification
+    // Whitout this, it would open undefined or the service worker file.
+    url = data.url ? data.url: self.location.origin;
 
   // Keep the service worker alive until the notification is created.
   event.waitUntil(
@@ -23,12 +24,12 @@ self.addEventListener('push', function(event) {
   );
 });
 
-// When user clicks in the notification, opens a new window with the url 
-// and closes the notification.
 self.addEventListener('notificationclick', function (event) {
   event.waitUntil(
-    self.clients.openWindow(url),
-    event.notification.close()
+    event.preventDefault(),
+    event.notification.close(),
+    self.clients.openWindow(url)
   );
+  console.log(url)
 })
 
