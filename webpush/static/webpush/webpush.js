6d494430f75798c6d745e5d0a93a89bc8fb6a125
Alex Bișag <bisagalexstefan@gmail.com>
2019-02-15 21:55:48 +0200

    Update webpush.js (#52)
    Added a check variable inside the localStorage which will stop the subscribe button to lose its state at page refresh.

--- webpush/static/webpush/webpush.js
+++ webpush/static/webpush/webpush.js
@@ -5,111 +5,142 @@ var isPushEnabled = false,
   messageBox,
   registration;
 
-window.addEventListener('load', function() {
+window.addEventListener('load', function () {
   subBtn = document.getElementById('webpush-subscribe-button');
   messageBox = document.getElementById('webpush-message');
 
+  if (this.localStorage.getItem('push_subscribed')) {
+    isPushEnabled = true;
+    subBtn.textContent = "Unsubscribe to Push Messaging";
+  }
+
   subBtn.addEventListener('click',
-    function() {
-      subBtn.disabled = true;
+    async function () {
       if (isPushEnabled) {
-        return unsubscribe()
+        registration = await initialiseServiceWorker();
+        return unsubscribe(registration)
+      } else {
+        registration = await initialiseServiceWorker();
+        return subscribe(registration)
       }
+    });
+});
 
-      // Do everything if the Browser Supports Service Worker
-      if ('serviceWorker' in navigator) { 
-        var serviceWorker = document.getElementById('service-worker-js').src;
-        navigator.serviceWorker.register(serviceWorker)
-          .then(
-            function(reg) {
-              subBtn.textContent = 'Loading....';
-              registration = reg;
-              initialiseState(reg);
-            }
-          );
-      }
-      // If service worker not supported, show warning to the message box
-      else {  
-        messageBox.textContent = 'Service Worker is not supported in your Browser!';
-        messageBox.style.display = 'block'; 
-      }
-    }
-  );
+async function initialiseServiceWorker() {
+  // Do everything if the Browser Supports Service Worker
+  if ('serviceWorker' in navigator) {
+    var serviceWorker = document.querySelector('meta[name="service-worker-js"]').content;
+    var register = await navigator.serviceWorker.register(serviceWorker)
+      .then(
+        function (reg) {
+          initialiseState(reg);
+          return reg;
+        }
+      );
 
-  // Once the service worker is registered set the initial state  
-  function initialiseState(reg) {
-    // Are Notifications supported in the service worker?  
-    if (!(reg.showNotification)) {
-        // Show a message and activate the button
-        messageBox.textContent = 'Showing Notification is not suppoted in your browser';
-        subBtn.textContent = 'Subscribe to Push Messaging';
-        messageBox.style.display = 'block';
-        return;
-    }
-
-    // Check the current Notification permission.  
-    // If its denied, it's a permanent block until the  
-    // user changes the permission  
-    if (Notification.permission === 'denied') {
-      // Show a message and activate the button
-      messageBox.textContent = 'The Push Notification is blocked from your browser.';
-      subBtn.textContent = 'Subscribe to Push Messaging';
-      subBtn.disabled = false;
-      messageBox.style.display = 'block';
-      return;  
-    }
-
-    // Check if push messaging is supported  
-    if (!('PushManager' in window)) {
-      // Show a message and activate the button 
-      messageBox.textContent = 'Push Notification is not available in the browser';
-      subBtn.textContent = 'Subscribe to Push Messaging';
-      subBtn.disabled = false;
-      messageBox.style.display = 'block';
-      return;  
-    }
-
-    // We need to subscribe for push notification and send the information to server  
-    subscribe(reg)
+    return register;
+  }
+  // If service worker not supported, show warning to the message box
+  else {
+    messageBox.textContent = 'Service Worker is not supported in your Browser!';
+    messageBox.style.display = 'block';
   }
 }
-);
 
+// Once the service worker is registered set the initial state  
+function initialiseState(reg) {
+  // Are Notifications supported in the service worker?  
+  if (!(reg.showNotification)) {
+    // Show a message and activate the button
+    messageBox.textContent = 'Showing Notification is not suppoted in your browser';
+    subBtn.textContent = 'Subscribe to Push Messaging';
+    messageBox.style.display = 'block';
+    return;
+  }
+
+  // Check the current Notification permission.  
+  // If its denied, it's a permanent block until the  
+  // user changes the permission  
+  if (Notification.permission === 'denied') {
+    // Show a message and activate the button
+    messageBox.textContent = 'The Push Notification is blocked from your browser.';
+    subBtn.textContent = 'Subscribe to Push Messaging';
+    subBtn.disabled = false;
+    messageBox.style.display = 'block';
+    return;
+  }
+
+  // Check if push messaging is supported  
+  if (!('PushManager' in window)) {
+    // Show a message and activate the button 
+    messageBox.textContent = 'Push Notification is not available in the browser';
+    subBtn.textContent = 'Subscribe to Push Messaging';
+    subBtn.disabled = false;
+    messageBox.style.display = 'block';
+    return;
+  }
+
+  // We need to subscribe for push notification and send the information to server  
+}
 
 function subscribe(reg) {
   // Get the Subscription or register one
-  getSubscription(reg)
-    .then(
-      function(subscription) {
-        postSubscribeObj('subscribe',subscription);
-      }
-    )
+  getSubscription(reg).then(
+    function (subscription) {
+      localStorage.setItem('push_subscribed', "1");
+      postSubscribeObj('subscribe', subscription);
+    }
+  )
     .catch(
-      function(error) {
+      function (error) {
         console.log('Subscription error.', error)
       }
     )
 }
 
-function getSubscription(reg) {
-    return reg.pushManager.getSubscription()
-      .then(
-        function(subscription) {
-          // Check if Subscription is available
-          if (subscription) {
-            return subscription;
-          }
-          // If not, register one
-          return registration.pushManager.subscribe({ userVisibleOnly: true });
-        }
-      )
+function urlB64ToUint8Array(base64String) {
+  const padding = '='.repeat((4 - base64String.length % 4) % 4);
+  const base64 = (base64String + padding)
+    .replace(/\-/g, '+')
+    .replace(/_/g, '/');
+
+  const rawData = window.atob(base64);
+  const outputArray = new Uint8Array(rawData.length);
+
+  for (var i = 0; i < rawData.length; ++i) {
+    outputArray[i] = rawData.charCodeAt(i);
+  }
+  return outputArray;
 }
 
-function unsubscribe() {
+function getSubscription(reg) {
+  return reg.pushManager.getSubscription().then(
+    function (subscription) {
+      var metaObj, applicationServerKey, options;
+      // Check if Subscription is available
+      if (subscription) {
+        return subscription;
+      }
+
+      metaObj = document.querySelector('meta[name="django-webpush-vapid-key"]');
+      applicationServerKey = metaObj.content;
+      options = {
+        userVisibleOnly: true
+      };
+      if (applicationServerKey) {
+        options.applicationServerKey = urlB64ToUint8Array(applicationServerKey)
+      }
+      // If not, register one
+      return registration.pushManager.subscribe(options)
+    }
+  )
+}
+
+function unsubscribe(registration) {
   // Get the Subscription to unregister
   registration.pushManager.getSubscription()
     .then(
-      function(subscription) {
+      function (subscription) {
 
         // Check we have a subscription to unsubscribe
         if (!subscription) {
@@ -120,9 +151,11 @@ function unsubscribe() {
           messageBox.style.display = 'block';
           return;
         }
+
+        localStorage.removeItem('push_subscribed');
         postSubscribeObj('unsubscribe', subscription);
       }
-    )  
+    )
 }
 
 function postSubscribeObj(statusType, subscription) {
@@ -130,22 +163,23 @@ function postSubscribeObj(statusType, subscription) {
   // the type of the request, the name of the user subscribing, 
   // and the push subscription endpoint + key the server needs
   // to send push messages
-  
+
   var browser = navigator.userAgent.match(/(firefox|msie|chrome|safari|trident)/ig)[0].toLowerCase(),
-    data = {  status_type: statusType,
-              subscription: subscription.toJSON(),
-              browser: browser,
-              group: subBtn.dataset.group
-           };
+    data = {
+      status_type: statusType,
+      subscription: subscription.toJSON(),
+      browser: browser,
+      group: subBtn.dataset.group
+    };
 
   fetch(subBtn.dataset.url, {
     method: 'post',
-    headers: {'Content-Type': 'application/json'},
+    headers: { 'Content-Type': 'application/json' },
     body: JSON.stringify(data),
     credentials: 'include'
   })
     .then(
-      function(response) {
+      function (response) {
         // Check the information is saved successfully into server
         if ((response.status == 201) && (statusType == 'subscribe')) {
           // Show unsubscribe button instead
@@ -161,22 +195,22 @@ function postSubscribeObj(statusType, subscription) {
           // Get the Subscription
           getSubscription(registration)
             .then(
-              function(subscription) {
+              function (subscription) {
                 // Remove the subscription
                 subscription.unsubscribe()
-                .then(
-                  function(successful) {
-                    subBtn.textContent = 'Subscribe to Push Messaging';
-                    messageBox.textContent = 'Successfully unsubscribed for Push Notification';
-                    messageBox.style.display = 'block';
-                    isPushEnabled = false;
-                    subBtn.disabled = false;
-                  }
-                )
+                  .then(
+                    function (successful) {
+                      subBtn.textContent = 'Subscribe to Push Messaging';
+                      messageBox.textContent = 'Successfully unsubscribed for Push Notification';
+                      messageBox.style.display = 'block';
+                      isPushEnabled = false;
+                      subBtn.disabled = false;
+                    }
+                  )
               }
             )
             .catch(
-              function(error) {
+              function (error) {
                 subBtn.textContent = 'Unsubscribe to Push Messaging';
                 messageBox.textContent = 'Error during unsubscribe from Push Notification';
                 messageBox.style.display = 'block';
