2490f573ef7561222b0f81d14d765fdcc4b4fd68
Chris Kang <chris.sm.kang542@gmail.com>
2019-06-03 11:49:06 +1200

    Fixed Sending tests from admin page (#61)
    * Fixed Sending tests from admin page
    
    * 410 Error when sending test to deprecated subscription deletes
    automatically

--- webpush/admin.py
+++ webpush/admin.py
@@ -5,15 +5,21 @@ from django.contrib import admin
 from .models import PushInformation
 from .utils import _send_notification
 
+from pywebpush import WebPushException
 
 class PushInfoAdmin(admin.ModelAdmin):
     list_display = ("__str__", "user", "subscription", "group")
     actions = ("send_test_message",)
 
     def send_test_message(self, request, queryset):
-        result = []
         payload = {"head": "Hey", "body": "Hello World"}
         for device in queryset:
-            result.append(_send_notification(device, json.dumps(payload), 0))
+            try:
+                _send_notification(device.subscription, json.dumps(payload), 0)
+            except WebPushException as ex:
+                device.delete()
+                self.message_user(request,"Deprecated subscription deleted")
+        self.message_user(request,"Test sent successfully")
+
 
 admin.site.register(PushInformation, PushInfoAdmin)
