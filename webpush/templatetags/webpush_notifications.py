54fe2cea15fc49abb3016e203a599513b94998bc
Evgeny Lyzov <elyzov@plesk.com>
2020-03-18 20:17:24 +0700

    Added ability to specify webpush button classes
    (cherry picked from commit 76df2fdcea1b4280bae8acdc56c36ec13f3be2a3)

--- webpush/templatetags/webpush_notifications.py
+++ webpush/templatetags/webpush_notifications.py
@@ -16,6 +16,7 @@ def webpush_header(context):
 
 @register.filter
 @register.inclusion_tag('webpush_button.html', takes_context=True)
-def webpush_button(context):
+def webpush_button(context, with_class):
     template_context = get_templatetag_context(context)
+    template_context['class'] = with_class
     return template_context
