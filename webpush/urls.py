f7f2e108ca597d7343c0f97008d83216a3b2c91e
Rebecca Claire Murphy <hi@rcm.dev>
2020-11-26 10:30:00 -0500

    Add i18n Support

--- webpush/urls.py
+++ webpush/urls.py
@@ -1,9 +1,22 @@
 from django.conf.urls import url
+from django.urls import path
+from django.views.i18n import JavaScriptCatalog
 
+from django.views.decorators.cache import cache_page
+from django.utils import timezone
 from . import views
 
+# When we last restarted the server; used for cache control headers and
+# invalidating the server side cache on server restart
+last_modified_date = timezone.now()
+
 urlpatterns = [
-    url(r'^save_information', views.save_info, name='save_webpush_info'),
+    path('jsi18n/',
+         cache_page(86400, key_prefix='js18n-%s' % last_modified_date)(
+             JavaScriptCatalog.as_view(packages=['webpush'])
+         ),
+         name='javascript-catalog'),
+    path('save_information', views.save_info, name='save_webpush_info'),
     # Service worker need to be loaded from same domain
-    url(r'^service-worker.js', views.ServiceWorkerView.as_view(), name='service_worker')
+    path('service-worker.js', views.ServiceWorkerView.as_view(), name='service_worker')
 ]
