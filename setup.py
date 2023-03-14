a3844f463f82847da796aefed9df2111ac27d1ab
Safwan Rahman <safwan.rahman15@gmail.com>
2023-03-14 23:59:38 +0600

    bumping version

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
     name='django-webpush',
-    version='0.3.4',
+    version='0.3.5',
     packages=find_packages(),
     include_package_data=True,
     license='GNU Public License',
