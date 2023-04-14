c25ab01199c7febb2e9823383ef837a76e7853a7
Cristian <cristiandeveloper4699@gmail.com>
2023-04-14 10:55:37 -0500

    Headers added to __init__.py file

--- webpush/__init__.py
+++ webpush/__init__.py
@@ -3,11 +3,11 @@ import json
 from .utils import send_notification_to_group, send_notification_to_user
 
 
-def send_group_notification(group_name, payload, ttl=0):
+def send_group_notification(group_name, payload, ttl=0, headers={}):
     payload = json.dumps(payload)
-    send_notification_to_group(group_name, payload, ttl)
+    send_notification_to_group(group_name, payload, ttl, headers=headers)
 
 
-def send_user_notification(user, payload, ttl=0):
+def send_user_notification(user, payload, ttl=0, headers={}):
     payload = json.dumps(payload)
-    send_notification_to_user(user, payload, ttl)
+    send_notification_to_user(user, payload, ttl, headers=headers)
