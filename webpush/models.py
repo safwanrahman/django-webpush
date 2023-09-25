1570baa07175e89c5809aeb59c653d4ac2f7bdfe
jialutu <jialutu@gmail.com>
2023-09-25 20:39:38 +0100

    updated SubscriptionInfo to allow user_agent blankable

--- webpush/models.py
+++ webpush/models.py
@@ -11,7 +11,7 @@ class Group(models.Model):
 
 class SubscriptionInfo(models.Model):
     browser = models.CharField(max_length=100)
-    user_agent = models.CharField(max_length=500)
+    user_agent = models.CharField(max_length=500, blank=True)
     endpoint = models.URLField(max_length=500)
     auth = models.CharField(max_length=100)
     p256dh = models.CharField(max_length=100)
