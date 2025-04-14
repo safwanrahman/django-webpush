4db0e6d2141af1c5d7cb574db6d9710be10b6fe7
David Guillot <david@guillot.me>
2025-04-14 09:33:34 +0200

    fix(command): wrong class name, command not detected

--- webpush/management/commands/webpush_generate_vapid_keypair.py
+++ webpush/management/commands/webpush_generate_vapid_keypair.py
@@ -3,7 +3,7 @@ from django.core.management.base import BaseCommand
 from ...vapid import get_vapid_keypair
 
 
-class GenerateValidKeys(BaseCommand):
+class Command(BaseCommand):
     def handle(self, *args, **options):
         pubkey, privkey = get_vapid_keypair()
         self.stdout.write(f"Public key: {pubkey}")
